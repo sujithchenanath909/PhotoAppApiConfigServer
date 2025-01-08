20429ca90054297f454daa5f3df646dc970a5a56
Sujith Chenanath <sujithchenanath@gmail.com>
2025-01-08 20:54:00 +0530

    first commit

---
+++ src/test/java/in/sujithchenanath/photoapp/api/gateway/PhotoAppApiConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package in.sujithchenanath.photoapp.api.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
